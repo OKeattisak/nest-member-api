b3faa17adc091cacf61b78e5ba3a6d48eb249d20
Keattisak <OKeattisak.A@outlook.com>
2025-09-14 11:20:58 +0700

    remove test

--- test/setup-e2e.ts
+++
@@ -1,9 +0,0 @@
-import { setupTestEnvironment } from './utils/test-setup';
-
-// Global setup for E2E tests
-beforeAll(async () => {
-  await setupTestEnvironment();
-});
-
-// Increase timeout for E2E tests
-jest.setTimeout(30000);
